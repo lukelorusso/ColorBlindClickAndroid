343bbec6150dd769c159fbd244e2126587f1efb0
Luke Lorusso <luca.lor17@gmail.com>
2021-03-21 12:30:50 +0100

    Refining presentation module

--- presentation/src/main/kotlin/com/lukelorusso/presentation/exception/ErrorMessageFactory.kt
+++ presentation/src/main/kotlin/com/lukelorusso/presentation/exception/ErrorMessageFactory.kt
@@ -4,32 +4,34 @@ import android.content.Context
 import com.lukelorusso.domain.exception.NoConnectedException
 import com.lukelorusso.domain.exception.PersistenceException
 import com.lukelorusso.domain.exception.WebServiceException
+import com.lukelorusso.domain.usecases.base.Logger
 import com.lukelorusso.presentation.R
-import timber.log.Timber
-import javax.inject.Inject
 
 /**
  * Copyright (C) 2020 Mikhael LOPEZ
  * Licensed under the Apache License Version 2.0
  * Factory used to create error messages from an Exception as a condition.
  */
-open class ErrorMessageFactory
-@Inject internal constructor(private val context: Context) {
+interface ErrorMessageFactory {
 
     /**
      * Creates a String representing an error message.
      * @param exception An exception used as a condition to retrieve the correct error message.
      * @return [String] an error message.
      */
-    open fun getError(exception: Throwable?): String =
-        exception?.let {
+    fun getError(exception: Throwable?): String
+
+    class Impl(private val context: Context, private val logger: Logger) : ErrorMessageFactory {
+        override fun getError(exception: Throwable?): String = exception?.let {
             when (it) {
                 is NoConnectedException -> context.getString(R.string.error_no_connection)
                 is WebServiceException -> context.getString(R.string.error_web_service)
                 is PersistenceException -> context.getString(R.string.error_persistence)
                 else -> exception.localizedMessage ?: context.getString(R.string.error_generic)
-            }.apply { Timber.e(it) }
+            }.apply { logger.logError { it } }
         } ?: getGenericError()
 
-    private fun getGenericError() = context.getString(R.string.error_generic)
+        private fun getGenericError() = context.getString(R.string.error_generic)
+    }
+
 }
