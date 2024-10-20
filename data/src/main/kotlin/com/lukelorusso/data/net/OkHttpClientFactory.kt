d9d68d1587723a76dc67159de291bb41432407b2
Luke Lorusso <luca.lor17@gmail.com>
2024-10-20 14:21:29 +0200

    Adding HttpLoggingInterceptor

--- data/src/main/kotlin/com/lukelorusso/data/net/OkHttpClientFactory.kt
+++ data/src/main/kotlin/com/lukelorusso/data/net/OkHttpClientFactory.kt
@@ -1,19 +1,37 @@
 package com.lukelorusso.data.net
 
 import android.content.Context
+import com.lukelorusso.data.BuildConfig
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 /**
- * Copyright (C) 2021 Luke Lorusso
+ * Copyright (C) 2024 Luke Lorusso
  * Licensed under the Apache License Version 2.0
  */
 open class OkHttpClientFactory {
 
     open fun createOkHttpClient(context: Context? = null): OkHttpClient =
         OkHttpClient.Builder()
-            .apply { updateTimeout() }
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    enableDebugTools(context)
+                }
+                updateTimeout()
+            }
             .build()
+
+
+    private fun OkHttpClient.Builder.enableDebugTools(context: Context?) {
+        context?.also {
+            addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                }
+            )
+        }
+    }
 
     private fun OkHttpClient.Builder.updateTimeout(read: Long = 60, write: Long = 60) {
         readTimeout(read, TimeUnit.SECONDS)
